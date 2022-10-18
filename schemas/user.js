26939b091ddaee63cbe315b84353be7fcb1fdb26
Andriy Makarov <angeltek69@gmail.com>
2022-10-18 21:07:39 +0200

    Fulfilled

---
+++ schemas/user.js
@@ -0,0 +1,29 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object({
+  name: Joi.string()
+    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+    .min(2)
+    .max(40)
+    .required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+    })
+    .required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+    })
+    .required(),
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+};
